aad64af330be827596bdbeb36c381a1abf6d83db
martin chan <martinchan994@gmail.com>
2020-02-01 21:18:55 +0100

    moved post recipe to app.js

--- app.js
+++ app.js
@@ -1,23 +1,34 @@
 const hH = new httpHandler;
 const uH = new uiHandler;
-
+const baseURL = "https://martinchan.pythonanywhere.com";
 /** Make the navbar navigate to corresponding page
  */
-document.getElementById("recipes-nav").addEventListener("click", uH.goToRecipesPage);
-document.getElementById("add-recipe-nav").addEventListener("click", uH.goToAddRecipePage);
-document.getElementById("login-nav").addEventListener("click", uH.goToLoginPage);
+document.getElementById('recipes-nav').addEventListener('click', uH.goToRecipesPage);
+document.getElementById('add-recipe-nav').addEventListener('click', uH.goToAddRecipePage);
+document.getElementById('login-nav').addEventListener('click', uH.goToLoginPage);
 
 /** Get the data from the database and show it.
  */
-hH.get("https://martinchan.pythonanywhere.com/recipes")
+hH.get(baseURL + "/recipes")
   .then(data => uH.showRecipes(data))
   .catch(err => console.log(err));
 
 /** Make the add button for the add-recipe-page
  *  add a new row when clicked.
  */
-document.getElementById("addIngredientButton").addEventListener("click", e => uH.addIngredientRow(e));
+document.getElementById('addIngredientButton').addEventListener('click', e => uH.addIngredientRow(e));
 
 /** Make the add recipe button submit the recipe to the cookbook
  */
-document.getElementById("submit-recipe-button").addEventListener("click", e => uH.submitRecipe(e, hH, "https://martinchan.pythonanywhere.com/addrecipe"));
+document.getElementById('submit-recipe-button').addEventListener('click', e => {
+  const recipe = uH.submitRecipe();
+  recipe['password'] = "Troglodon5986";
+  hH.post(baseURL + "/addrecipe", recipe)
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+  e.preventDefault();
+});
+
+
+
+
--- ui.js
+++ ui.js
@@ -1,44 +1,44 @@
 class uiHandler {
   goToRecipesPage() {
-    document.getElementById("search-recipes").style.display = "block";
-    document.getElementById("recipes").style.display = "block";
-    document.getElementById("single-recipe").style.display = "none";
-    document.getElementById("add-recipes").style.display = "none";
-    document.getElementById("login-container").style.display = "none";
+    document.getElementById('search-recipes').style.display = 'block';
+    document.getElementById('recipes').style.display = 'block';
+    document.getElementById('single-recipe').style.display = 'none';
+    document.getElementById('add-recipes').style.display = 'none';
+    document.getElementById('login-container').style.display = 'none';
   }
   
   goToAddRecipePage() {
-    document.getElementById("search-recipes").style.display = "none";
-    document.getElementById("recipes").style.display = "none";
-    document.getElementById("single-recipe").style.display = "none";
-    document.getElementById("add-recipes").style.display = "block";
-    document.getElementById("login-container").style.display = "none";
+    document.getElementById('search-recipes').style.display = 'none';
+    document.getElementById('recipes').style.display = 'none';
+    document.getElementById('single-recipe').style.display = 'none';
+    document.getElementById('add-recipes').style.display = 'block';
+    document.getElementById('login-container').style.display = 'none';
   }
 
   goToLoginPage() {// TODO
   }
 
   showRecipe(e, recipe) {
-    document.getElementById("search-recipes").style.display = "none";
-    document.getElementById("recipes").style.display = "none";
-    document.getElementById("single-recipe").style.display = "block";
-    document.getElementById("add-recipes").style.display = "none";
-    document.getElementById("login-container").style.display = "none";
+    document.getElementById('search-recipes').style.display = 'none';
+    document.getElementById('recipes').style.display = 'none';
+    document.getElementById('single-recipe').style.display = 'block';
+    document.getElementById('add-recipes').style.display = 'none';
+    document.getElementById('login-container').style.display = 'none';
 
-    document.getElementById("recipe-name-show").innerHTML = recipe.name;
-    document.getElementById("servings-show").innerHTML = "Servings: " + recipe.servings;
-    document.getElementById("description-show").innerHTML = recipe.description;
+    document.getElementById('recipe-name-show').innerHTML = recipe.name;
+    document.getElementById('servings-show').innerHTML = "Servings: " + recipe.servings;
+    document.getElementById('description-show').innerHTML = recipe.description;
 
-    const ingredientsList = document.getElementById("ingredients-list-show");
+    const ingredientsList = document.getElementById('ingredients-list-show');
     while (ingredientsList.lastChild) {
       ingredientsList.removeChild(ingredientsList.lastChild);
     }
 
     let ingItem ;
     recipe.ingredients.forEach(ing => {
-      ingItem = document.createElement("li");
+      ingItem = document.createElement('li');
       ingItem.innerHTML = ing.name + ", " + ing.amount + " " + ing.unit;
-      document.getElementById("ingredients-list-show").appendChild(ingItem);
+      document.getElementById('ingredients-list-show').appendChild(ingItem);
     });
 
     e.preventDefault();
@@ -51,22 +51,22 @@ class uiHandler {
     let cardBody;
     let descriptionText;
     recipes.forEach(recipe => {
-      recipeCard = document.createElement("div");
-      recipeCard.classList.add("card");
-      recipeCard.classList.add("w-75");
-      recipeCard.classList.add("mt-3");
+      recipeCard = document.createElement('div');
+      recipeCard.classList.add('card');
+      recipeCard.classList.add('w-75');
+      recipeCard.classList.add('mt-3');
 
-      header = document.createElement("h5");
-      header.classList.add("card-header");
+      header = document.createElement('h5');
+      header.classList.add('card-header');
       header.appendChild(document.createTextNode(recipe.name));
-      header.style.textAlign = "left";
+      header.style.textAlign = 'left';
 
-      cardBody = document.createElement("div");
-      cardBody.classList.add("card-body");
-      cardBody.style.textAlign = "left";
+      cardBody = document.createElement('div');
+      cardBody.classList.add('card-body');
+      cardBody.style.textAlign = 'left';
 
-      descriptionText = document.createElement("p");
-      descriptionText.classList.add("card-text");
+      descriptionText = document.createElement('p');
+      descriptionText.classList.add('card-text');
       descriptionText.appendChild(document.createTextNode(recipe.description));
 
       cardBody.appendChild(descriptionText);
@@ -74,58 +74,58 @@ class uiHandler {
       recipeCard.appendChild(header);
       recipeCard.appendChild(cardBody);
 
-      recipeCard.style.cursor = "pointer";
-      recipeCard.addEventListener("click", e => this.showRecipe(e, recipe));
-      document.getElementById("recipes").appendChild(recipeCard);
+      recipeCard.style.cursor = 'pointer';
+      recipeCard.addEventListener('click', e => this.showRecipe(e, recipe));
+      document.getElementById('recipes').appendChild(recipeCard);
 
     });
   }
 
   addIngredientRow(e) {
     e.preventDefault();
-    const rowDiv = document.createElement("div");
-    rowDiv.classList.add("form-row");
-    rowDiv.classList.add("flex-nowrap");
+    const rowDiv = document.createElement('div');
+    rowDiv.classList.add('form-row');
+    rowDiv.classList.add('flex-nowrap');
 
-    const ingredientDiv = document.createElement("div");
-    const unitDiv = document.createElement("div");
-    const amountDiv = document.createElement("div");
-    const minusDiv = document.createElement("div");
+    const ingredientDiv = document.createElement('div');
+    const unitDiv = document.createElement('div');
+    const amountDiv = document.createElement('div');
+    const minusDiv = document.createElement('div');
     
-    ingredientDiv.classList.add("form-group");
-    ingredientDiv.classList.add("col-md-7");
+    ingredientDiv.classList.add('form-group');
+    ingredientDiv.classList.add('col-md-7');
 
-    amountDiv.classList.add("form-group");
-    amountDiv.classList.add("col-md-2");
+    amountDiv.classList.add('form-group');
+    amountDiv.classList.add('col-md-2');
 
-    unitDiv.classList.add("form-group");
-    unitDiv.classList.add("col-md-2");
+    unitDiv.classList.add('form-group');
+    unitDiv.classList.add('col-md-2');
 
-    minusDiv.classList.add("form-group");
-    minusDiv.classList.add("col-md-1");
+    minusDiv.classList.add('form-group');
+    minusDiv.classList.add('col-md-1');
 
-    const ingredientInput = document.createElement("input");
-    const amountInput = document.createElement("input");
-    const unitInput = document.createElement("input");
-    const minusButton = document.createElement("button");
+    const ingredientInput = document.createElement('input');
+    const amountInput = document.createElement('input');
+    const unitInput = document.createElement('input');
+    const minusButton = document.createElement('button');
 
-    ingredientInput.classList.add("form-control");
-    ingredientInput.id = "ingredients-input";
+    ingredientInput.classList.add('form-control');
+    ingredientInput.id = 'ingredients-input';
 
-    amountInput.classList.add("form-control");
-    amountInput.id = "amount-input";
-    amountInput.setAttribute("type", "number");
+    amountInput.classList.add('form-control');
+    amountInput.id = 'amount-input';
+    amountInput.setAttribute('type', 'number');
 
-    unitInput.classList.add("form-control");
-    unitInput.id = "units-input";
-    unitInput.setAttribute("list", "unitList");
+    unitInput.classList.add('form-control');
+    unitInput.id = 'units-input';
+    unitInput.setAttribute('list', 'unitList');
 
-    minusButton.classList.add("btn");
-    minusButton.classList.add("btn-primary");
-    minusButton.classList.add("btn-danger");
-    minusButton.classList.add("w-100");
-    minusButton.setAttribute("type", "submit");
-    minusButton.innerHTML = "-";
+    minusButton.classList.add('btn');
+    minusButton.classList.add('btn-primary');
+    minusButton.classList.add('btn-danger');
+    minusButton.classList.add('w-100');
+    minusButton.setAttribute('type', 'submit');
+    minusButton.innerHTML = '-';
 
     ingredientDiv.appendChild(ingredientInput);
     amountDiv.appendChild(amountInput);
@@ -137,28 +137,28 @@ class uiHandler {
     rowDiv.appendChild(unitDiv);
     rowDiv.append(minusDiv);
 
-    minusButton.addEventListener("click", e => this.removeIngredientRow(e, rowDiv));
+    minusButton.addEventListener('click', e => this.removeIngredientRow(e, rowDiv));
 
-    const ingredientList = document.getElementById("ingredient-list");
+    const ingredientList = document.getElementById('ingredient-list');
     let childrenNumber = ingredientList.children.length;
     ingredientList.insertBefore(rowDiv, ingredientList.childNodes[childrenNumber]);
     
   }
 
   removeIngredientRow(e, rowDiv) {
-    document.getElementById("ingredient-list").removeChild(rowDiv);
+    document.getElementById('ingredient-list').removeChild(rowDiv);
     e.preventDefault();
   }
 
   submitRecipe(e, hH, url) {
     let recipe = {};
-    const recipeName = document.getElementById("recipe-name-input").value;
-    const description = document.getElementById("recipe-description-input").value;
-    const servings = document.getElementById("servings-input").value;
+    const recipeName = document.getElementById('recipe-name-input').value;
+    const description = document.getElementById('recipe-description-input').value;
+    const servings = document.getElementById('servings-input').value;
 
-    const ingredients = document.querySelectorAll("#ingredients-input");
-    const amounts = document.querySelectorAll("#amount-input");
-    const units = document.querySelectorAll("#units-input");
+    const ingredients = document.querySelectorAll('#ingredients-input');
+    const amounts = document.querySelectorAll('#amount-input');
+    const units = document.querySelectorAll('#units-input');
 
     let ingObjects = [];
     let ing;
@@ -171,16 +171,11 @@ class uiHandler {
       ingObjects.push(ing);
     }
 
-    recipe["name"] = recipeName;
-    recipe["servings"] = servings;
-    recipe["description"] = description;
-    recipe["ingredients"] = ingObjects;
-    recipe["password"] = "Troglodon5986";
+    recipe['name'] = recipeName;
+    recipe['servings'] = servings;
+    recipe['description'] = description;
+    recipe['ingredients'] = ingObjects;
 
-    hH.post(url, recipe)
-      .then(data => console.log(data))
-      .catch(err => console.log(err));
-
-    e.preventDefault();
+    return recipe;
   }
 }
